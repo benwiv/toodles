38cbb2c35885c810991c3404bcba1b0a1bff2b59
Ben Waller <benwalleriv@gmail.com>
2019-08-14 13:13:06 -0700

    updated with date storage, add todo functionality, WIP complete a todo

--- toodles-app/src/App.jsx
+++ toodles-app/src/App.jsx
@@ -10,21 +10,25 @@ import './App.css';
 
 const App = () => {
 
+  const [ idCount, setIdCount ] = useState(1006);
   const [ toods, setToods ] = useState([
     {
       id: 1001,
       text: 'walk dog, even if he is not real',
       status: 'incomplete',
+      dueDate: '09/25/19',
     },
     {
       id: 1003,
       text: 'water plants, they are your friends',
       status: 'incomplete',
+      dueDate: '09/04/19',
     },
     {
       id: 1005,
       text: 'clip toenails',
       status: 'incomplete',
+      dueDate: '08/15/19',
     },
   ]);
 
@@ -33,19 +37,46 @@ const App = () => {
       id: 1002,
       text: 'pad cat bc she is good girl',
       status: 'complete',
+      dueDate: '08/12/19',
     },
     {
       id: 1004,
       text: 'disappear for a while',
       status: 'complete',
+      dueDate: '08/11/19',
     },
   ]);
+
+  const handleNewTood = ( text, date ) => {
+    return setToods([
+      ...toods, {
+        id: ( () => {
+          let id =  idCount;
+          setIdCount(idCount+1);
+          return id;
+        }),
+        text: text,
+        status: 'incomplete',
+        dueDate: date,
+      }
+    ])
+  };
+
+  const handleCompleteTood = (id) => {
+    const toodIndex = toods.findIndex( tood => tood.id.toString() === id);
+    console.log(toodIndex);
+    const newToodList = toods.splice(toodIndex, 1);
+    console.log(newToodList);
+    return setToods(newToodList);
+  }
 
   return (
     <section className="App">
       <ToodsList
         toods={toods}
         toodles={toodles}
+        handleNewTood={handleNewTood}
+        handleCompleteTood={handleCompleteTood}
       />
       <section className='app__container--right'>
         <Settings
--- toodles-app/src/Components/ToodsList/ToodsList.jsx
+++ toodles-app/src/Components/ToodsList/ToodsList.jsx
@@ -1,19 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './ToodsList.css';
 
-const ToodsList = ({ toods, toodles }) => {
+const ToodsList = ({ toods, toodles, handleNewTood, handleCompleteTood }) => {
+
+  const [ newToodText, setNewToodText ] = useState('');
+  const [ newToodDate, setNewToodDate ] = useState('');
+
   return (
     <div>
       <h1>toods</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleNewTood( newToodText, newToodDate );
+        }}
+        className='toodslist__form'
+      >
+        <div
+          className='toodslist__container--tood'
+        >
+          <div className='toodslist__checkbox--tood'></div>
+          <input
+            className='toodslist__input--tood'
+            placeholder='type tood here'
+            onChange={ e => {
+              setNewToodText(e.target.value);
+            }}
+          />
+          <div className='toodslist__container--date'>
+            <label for='toodDate'>due date</label>
+            <input
+              className='toodslist__input--tood'
+              placeholder='MM/DD/YY'
+              name='toodDate'
+              onChange={ e => {
+                setNewToodDate(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+        <button
+          className='toodslist__button--add pointer'
+          type='submit'
+        >add-a-tood</button>
+      </form>
       {toods.map((tood) => {
         return (
           <div
             key={tood.id}
-            className='toodslist__tood--container'
+            className='toodslist__container--tood'
           >
-            <div className='toodslist__tood--checkbox'></div>
-            <p className='toodslist__tood--text'>{tood.text}</p>
+            <div
+              className='toodslist__checkbox--tood pointer'
+              data-id={tood.id}
+              onClick={(e) => {
+                let toodId = e.target.dataset.id;
+                handleCompleteTood(toodId)
+              }}
+            ></div>
+            <p className='toodslist__text--tood'>{tood.text}</p>
           </div>
         )
       })}
@@ -22,10 +68,16 @@ const ToodsList = ({ toods, toodles }) => {
         return (
           <div
             key={toodle.id}
-            className='toodslist__toodles--container'
+            className='toodslist__container--tood'
           >
-            <div className='toodslist__tood--checkbox'></div>
-            <p className='toodslist__tood--text'>{toodle.text}</p>
+            <div className='toodslist__checkbox--tood pointer'>
+              <span
+                className='toodslist__checkmark--tood'
+                role='img'
+                aria-label='heavy check mark'
+              >✔️</span>
+            </div>
+            <p className='toodslist__text--tood'>{toodle.text}</p>
           </div>
         )
       })}
